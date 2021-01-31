6c3f3fff91637a930a6841a04561476d487a618d
minu27 <minal.vaity95@gmail.com>
2021-01-31 04:28:04 -0600

    Author routes - view,edit,delete

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const expresslayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
@@ -14,8 +15,8 @@ const bookRouter = require('./routes/books')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-
 app.use(expresslayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
